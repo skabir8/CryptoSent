6160a6b77b22d4eb101aa1f263e546d8379a4d6d
skabir8 <shariar.k8@gmail.com>
2018-06-08 11:20:11 -0400

    rem

--- utils/twitter_search.py
+++ utils/twitter_search.py
@@ -1,9 +1,9 @@
 import twitter
 
-api = twitter.Api(consumer_key='vdbaYse0QnknbPGOCFRvG5Y60',
-                      consumer_secret='ZVWpCARqpE8llzFOH4j875CiwZykJ7Nhg3ccDSOcE6ro6IXjhW',
-                      access_token_key='911995423690231809-5jA2hmYgP8qAAHwDyyHifjvlDEDV3en',
-                      access_token_secret='Sqazu1ta7PWois0MiW2krZ155D7M0N8MHchFTwQ0PFUyn')
+api = twitter.Api(consumer_key='',
+                      consumer_secret='',
+                      access_token_key='',
+                      access_token_secret='')
 def get_tweets(coin_name):
     ret_list = []
     query = "q=" + coin_name + "&result_type=recent&count=100"
